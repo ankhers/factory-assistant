f6bb6bc94c2a355f71a3823491d088a7bdef4431
Justin Wood <justin.k.wood@protonmail.com>
2019-04-05 09:05:28 -0400

    Use a different format for record definitions

--- src/types.ml
+++ src/types.ml
@@ -40,9 +40,9 @@ module Production = Map.Make(struct type t = part let compare = compare end)
 
 (* MODEL *)
 type production =
-  { building : production_building;
-    output : float;
-    input : (part * float) list;
+  { building : production_building
+  ; output : float
+  ; input : (part * float) list
   }
 
 let smelter = { building = Smelter; power_consumption = 4.}
@@ -55,59 +55,76 @@ let foundry = { building = Foundry; power_consumption = 16.}
 
 let prod =
   Production.empty
-    |> Production.add IronIngot { building = smelter;
-                                  output = 30.;
-                                  input = [] }
-    |> Production.add CopperIngot { building = smelter;
-                                    output = 30.;
-                                    input = [] }
+  |> Production.add IronIngot { building = smelter
+                              ; output = 30.
+                              ; input = []
+                              }
+  |> Production.add CopperIngot { building = smelter
+                                ; output = 30.
+                                ; input = []
+                                }
 
-    |> Production.add IronPlate { building = constructor;
-                                  output = 15.;
-                                  input = [(IronIngot, 30.)] }
-    |> Production.add IronRod { building = constructor;
-                                output = 15.;
-                                input = [(IronIngot, 15.)] }
-    |> Production.add Wire { building = constructor;
-                             output = 45.;
-                             input = [(CopperIngot, 15.)] }
-    |> Production.add Cable { building = constructor;
-                              output = 15.;
-                              input = [(Wire, 30.)] }
-    |> Production.add Concrete { building = constructor;
-                                 output = 15.;
-                                 input = [] }
-    |> Production.add Screw { building = constructor;
-                              output = 90.;
-                              input = [(IronRod, 15.)] }
-    |> Production.add SteelBeam { building = constructor;
-                                  output = 10.;
-                                  input = [(SteelIngot, 30.)] }
-    |> Production.add SteelPipe { building = constructor;
-                                  output = 15.;
-                                  input = [(SteelIngot, 15.)] }
+  |> Production.add IronPlate { building = constructor
+                              ; output = 15.
+                              ; input = [(IronIngot, 30.)]
+                              }
+  |> Production.add IronRod { building = constructor
+                            ; output = 15.
+                            ; input = [(IronIngot, 15.)]
+                            }
+  |> Production.add Wire { building = constructor
+                         ; output = 45.
+                         ; input = [(CopperIngot, 15.)]
+                         }
+  |> Production.add Cable { building = constructor
+                          ; output = 15.
+                          ; input = [(Wire, 30.)]
+                          }
+  |> Production.add Concrete { building = constructor
+                             ; output = 15.
+                             ; input = []
+                             }
+  |> Production.add Screw { building = constructor
+                          ; output = 90.
+                          ; input = [(IronRod, 15.)]
+                          }
+  |> Production.add SteelBeam { building = constructor
+                              ; output = 10.
+                              ; input = [(SteelIngot, 30.)]
+                              }
+  |> Production.add SteelPipe { building = constructor
+                              ; output = 15.
+                              ; input = [(SteelIngot, 15.)]
+                              }
 
-    |> Production.add ReinforcedIronPlate { building = assembler;
-                                            output = 5.;
-                                            input = [(IronPlate, 20.); (Screw, 120.)] }
-    |> Production.add Rotor { building = assembler;
-                              output = 6.;
-                              input = [(IronRod, 16.); (Screw, 132.)] }
-    |> Production.add ModularFrame { building = assembler;
-                                     output = 4.;
-                                     input = [(ReinforcedIronPlate, 12.); (IronRod, 24.)] }
-    |> Production.add EncasedIndustrialBeam { building = assembler;
-                                              output = 4.;
-                                              input = [(SteelBeam, 16.); (Concrete, 20.)] }
-    |> Production.add Stator { building = assembler;
-                               output = 6.;
-                               input = [(SteelPipe, 18.); (Wire, 60.)] }
-    |> Production.add Motor { building = assembler;
-                              output = 5.;
-                              input = [(Rotor, 10.); (Stator, 10.)] }
+  |> Production.add ReinforcedIronPlate { building = assembler
+                                        ; output = 5.
+                                        ; input = [(IronPlate, 20.); (Screw, 120.)]
+                                        }
+  |> Production.add Rotor { building = assembler
+                          ; output = 6.
+                          ; input = [(IronRod, 18.); (Screw, 132.)]
+                          }
+  |> Production.add ModularFrame { building = assembler
+                                 ; output = 4.
+                                 ; input = [(ReinforcedIronPlate, 12.); (IronRod, 24.)]
+                                 }
+  |> Production.add EncasedIndustrialBeam { building = assembler
+                                          ; output = 4.
+                                          ; input = [(SteelBeam, 16.); (Concrete, 20.)]
+                                          }
+  |> Production.add Stator { building = assembler
+                           ; output = 6.
+                           ; input = [(SteelPipe, 18.); (Wire, 60.)]
+                           }
+  |> Production.add Motor { building = assembler
+                          ; output = 5.
+                          ; input = [(Rotor, 10.); (Stator, 10.)]
+                          }
 
-    |> Production.add SteelIngot { building = foundry;
-                                   output = 30.;
-                                   input = [] }
+  |> Production.add SteelIngot { building = foundry
+                               ; output = 30.
+                               ; input = []
+                               }
 
 let initial_parts = [(IronIngot, 30.)]
